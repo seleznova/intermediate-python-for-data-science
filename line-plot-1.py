ba6a867ffd0c7cd179c65fb2e3a900f22d312854
seleznova <mary.kuzmenko@gmail.com>
2016-10-05 19:55:20 +0300

    Update line-plot-1.py

--- line-plot-1.py
+++ line-plot-1.py
@@ -22,3 +22,14 @@ plt.plot(gdp_cap, life_exp)
 
 # Display the plot
 plt.show()
+
+# Exercise 3
+
+# Change the line plot below to a scatter plot
+plt.scatter(gdp_cap, life_exp)
+
+# Put the x-axis on a logarithmic scale
+plt.xscale('log')
+
+# Show plot
+plt.show()
