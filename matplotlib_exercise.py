685aa859a08dfa0ed32bd26e793bff0df1733679
seleznova <mary.kuzmenko@gmail.com>
2016-10-05 19:52:48 +0300

    Add line-plot-1.py

--- matplotlib_exercise.py
+++ matplotlib_exercise.py
@@ -7,3 +7,11 @@ pop = [2.519, 3.692, 5.263, 6.972]
 plt.plot(year, pop)
 
 plt.show()
+
+year = [1950, 1970, 1990, 2010]
+
+pop = [2.519, 3.692, 5.263, 6.972]
+
+plt.scatter(year, pop)
+
+plt.show()
